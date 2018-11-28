509ffd80c24d6a9ebf4bf3cca80dde5043b55c73
prajiltp <tpprajilkottur@gmail.com>
2018-11-28 22:36:15 +0530

    mailer setup

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ module UsedCar
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      authentication: 'plain',
+      user_name: Rails.application.credentials[:gmail_username],
+      password: Rails.application.credentials[:gmail_password]
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
