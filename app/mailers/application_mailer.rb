5319822fa6fc53697516830802d25b1fa0038f23
prajiltp <tpprajilkottur@gmail.com>
2018-11-28 22:40:40 +0530

    mailer on create new car

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def subscriber_notification(car, emails)
+    @car = car
+     mail(
+      bcc: emails,
+      subject: "There is a new car added in #{@car.category.titleize}"
+    )
+  end
 end
